167df27a0d6a195ed4ffb9f0de4ad27465e56c95
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-05-31 09:32:00 -0400

    update template

--- templates/general.cpp
+++ templates/general.cpp
@@ -1,24 +1,7 @@
-#include <iostream>
-#include <vector>
-#include <unordered_set>
-#include <unordered_map>
-#include <set>
-#include <map>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-#define ll long long
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define vi vector<int>
-#define si set<int>
-#define usi unordered_set<int>
-#define mii map<int, int>
-#define umii unordered_map<int, int>
-#define pb push_back
-#define eb emplace_back
-#define fi first
-#define se second
 #define scan(x) do{while((_n=getchar())<45);if(_n-45)x=_n;else x=getchar();for(x-=48;47<(_=getchar());x=(x<<3)+(x<<1)+_-48);if(_n<46)x=-x;}while(0)
 
 signed main() {
