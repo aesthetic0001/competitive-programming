601f7c717fdf7758808268aaa0dc8957130d5bc1
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-07-11 19:30:56 -0400

    a

--- templates/general.cpp
+++ templates/general.cpp
@@ -2,9 +2,6 @@
 
 using namespace std;
 
-#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
-char _;
-
 signed main() {
     cin.tie(0); cin.sync_with_stdio(0);
 
