97c08264b90c994ee5ac373f9ef7389ae98fc887
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-08-06 12:13:35 -0400

    fix

--- DMOJ/coci14c6p3.cpp
+++ DMOJ/coci14c6p3.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#include <cassert>
+#include <cstdint>
 
 using namespace std;
 
@@ -21,41 +23,29 @@ signed main() {
     int maxShift = INT32_MAX;
 
     for (int col = 0; col < C; col++) {
-        int meteor = -1, ground = R - 1;
+        int meteor = -9999999, ground = R - 1;
         for (int i = 0; i < R; i++) {
-            // must be connected
-            if (meteor != -1 && photo[i][col] != 'X') break;
-            if (photo[i][col] == 'X') {
-                meteor = i;
-            }
+            if (photo[i][col] == 'X') meteor = i;
         }
         for (int i = R - 1; i >= 0; i--) {
-            if (ground != R - 1 && photo[i][col] != '#') break;
-            if (photo[i][col] == '#') {
-                ground = i;
-            }
+            if (photo[i][col] == '#') ground = i;
         }
-        // cout << ground << " " << meteor << " " << col << "\n";
         maxShift = min(maxShift, ground - meteor - 1);
     }
-
-    // cout << maxShift;
 
     for (int i = 0; i < R; i++) {
         for (int j = 0; j < C; j++) {
             if (photo[i][j] == 'X') {
-                // cout << "on skib bro\n";
                 ans[i + maxShift][j] = 'X';
-                ans[i][j] = '.';
-            } else if (!ans[i][j]) {
-                ans[i][j] = photo[i][j];
+            } else if (photo[i][j] == '#') {
+                ans[i][j] = '#';
             }
         }
     }
 
     for (int i = 0; i < R; i++) {
         for (int j = 0; j < C; j++) {
-            cout << ans[i][j];
+            cout << (ans[i][j] ? ans[i][j] : '.');
         }
         cout << "\n";
     }
