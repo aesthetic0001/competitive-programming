aeaf041e2c891e2b98d3ca7f7d0f9a30bbf5713d
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-08-22 17:39:12 -0400

    agony

--- CCC/ccc23s3.cpp
+++ CCC/ccc23s3.cpp
@@ -1,16 +1,84 @@
 #include <bits/stdc++.h>
-#define MAX 2000
+#include <cinttypes>
 
 using namespace std;
 
-#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
-char _;
-
-int v[MAX][MAX], N, M, R, C;
+bool ans[2001][2001];
+int N, M, R, C;
 
 signed main() {
     cin.tie(0); cin.sync_with_stdio(0);
+
     cin >> N >> M >> R >> C;
+
+    // more complex method to not have interference
+    if (R == N) {
+        if (M % 2 == 1) {
+            // odd - can form every combo
+            if (C % 2 == 1) {
+                for (int i = 0; i < ((M - C) / 2); i++) {
+                    ans[N - 1][i] = true;
+                    ans[N - 1][M - i - 1] = true;
+                }
+            } else {
+                for (int i = 0; i < ((M - C - 1) / 2); i++) {
+                    ans[N - 1][i] = true;
+                    ans[N - 1][M - i - 1] = true;
+                }
+                ans[N - 1][(M - 1) / 2] = true;
+            }
+        } else {
+            // even - only even C are possible
+            if (C % 2 == 1) {
+                cout << "IMPOSSIBLE";
+                return 0;
+            }
+            for (int i = 0; i < ((M - C) / 2); i++) {
+                ans[N - 1][i] = true;
+                ans[N - 1][M - i - 1] = true;
+            }
+        }
+    } else if (C == M) {
+        if (N % 2 == 1) {
+            // odd - can form every combo
+            if (R % 2 == 1) {
+                for (int i = 0; i < ((N - R) / 2); i++) {
+                    ans[i][M - 1] = true;
+                    ans[N - i - 1][M - 1] = true;
+                }
+            } else {
+                for (int i = 0; i < ((N - R - 1) / 2); i++) {
+                    ans[i][M - 1] = true;
+                    ans[N - i - 1][M - 1] = true;
+                }
+                ans[(N - 1) / 2][M - 1] = true;
+            }
+        } else {
+            // even - only even C are possible
+            if (R % 2 == 1) {
+                cout << "IMPOSSIBLE";
+                return 0;
+            }
+            for (int i = 0; i < ((N - R) / 2); i++) {
+                ans[i][M - 1] = true;
+                ans[N - i - 1][M - 1] = true;
+            }
+        }
+    } else {
+        // basic method when interference is not guaranteed
+        for (int i = R; i < N; i++) {
+            for (int j = C; j < M; j++) {
+                ans[i][j] = true;
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cout << (ans[i][j] ? 'b' : 'a');
+        }
+        cout << '\n';
+    }
 
     return 0;
 }
