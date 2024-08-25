47c9ee0d157ab66cdfabb3452871333c298bca79
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-08-24 20:45:40 -0400

    added dmopc21c2p1

--- DMOJ/dmopc21c2p1.cpp
+++ DMOJ/dmopc21c2p1.cpp
@@ -1,20 +1,6 @@
 #include <bits/stdc++.h>
-#include <cstdint>
-#include <cstdio>
 
 #define int long long
-#ifdef fread_unlocked
-#define fread fread_unlocked
-#endif
-#define INPUT_SIZE (4<<20)
-int _i0=0;
-char _i[INPUT_SIZE+5];
-#define su(x) do{while((_=_i[_i0++])<48);for(x=_-48;47<(_=_i[_i0++]);x=x*10+_-48);}while(0)
-#define si(x) do{while((_n=_i[_i0++])<45);if(_n-45)x=_n;else x=_i[_i0++];for(x-=48;47<(_=_i[_i0++]);x=x*10+_-48);if(_n<46)x=-x;}while(0)
-#define su2(x) do{for(x=_i[_i0++]-48;47<(_=_i[_i0++]);x=x*10+_-48);}while(0)
-#define si2(x) do{_n=_i[_i0++];if(_n-45)x=_n;else x=_i[_i0++];for(x-=48;47<(_=_i[_i0++]);x=x*10+_-48);if(_n<46)x=-x;}while(0)
-#define ss(x) do{int i=0; while((_=_i[_i0++])<=32); for(; _>32; _= _i[_i0++]) x[i++]=_; x[i]='\0';}while(0)
-char _,_n;
 
 #define eb(a) emplace_back(a)
 #define em(a) emplace(a)
@@ -31,12 +17,11 @@ signed main() {
     #ifdef LOCAL
     freopen("sample.in","r",stdin);
     #endif
-    fread(_i,1,INPUT_SIZE,stdin);
 
-    su2(N); su2(H); su2(P);
+    scanf("%lld %lld %lld", &N, &H, &P);
 
     for (int i = 0; i < N; i++) {
-        su2(temp1);
+        scanf("%lld", &temp1);
         lvlSum += temp1;
         lvlMax = max(temp1, lvlMax);
         pq.em(temp1);
@@ -46,7 +31,7 @@ signed main() {
 
     // lvlmax is at most 10^6
     for (int x = 0; x <= lvlMax; x++) {
-        while (x > pq.top()) {
+        while (!pq.empty() && x > pq.top()) {
             pq.pop();
         }
         if (x > 0) permCancel += sz(pq);
