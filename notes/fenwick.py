ddb2e0f65010f2186d89deed957c039cfd508a71
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-08-29 21:47:27 -0400

    add fenwick

---
+++ notes/fenwick.py
@@ -0,0 +1,26 @@
+original = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# one indexed for simplicity's sake
+fenwick = [0 for _ in range(len(original) + 1)]
+
+# fenwick functions
+def query(idx):
+    ans = 0
+    while idx > 0:
+        ans += fenwick[idx]
+        idx -= (idx & -idx)
+    return ans
+
+def update(idx, delta):
+    while idx < len(fenwick):
+        fenwick[idx] += delta
+        idx += (idx & -idx)
+
+def sum(l, r):
+    return query(r) - query(l - 1)
+
+
+for i in range(len(original)):
+    update(i + 1, original[i])
+
+print(sum(3, 4))
