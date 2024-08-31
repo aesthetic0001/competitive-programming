354d87f530f900951e68d9d35ae1907fcfe99686
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-08-31 17:57:45 -0400

    add

---
+++ DMOJ/ds2.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+// if segfaults, try using cstdio (scanf and print) because the buffered input may be tweaking
+
+// #define int long long
+#ifdef fread_unlocked
+#define fread fread_unlocked
+#endif
+#define INPUT_SIZE (4<<20)
+int _i0=0;
+char _i[INPUT_SIZE+5];
+#define su(x) do{for(x=_i[_i0++]-48;47<(_=_i[_i0++]);x=x*10+_-48);}while(0)
+#define si(x) do{_n=_i[_i0++];if(_n-45)x=_n;else x=_i[_i0++];for(x-=48;47<(_=_i[_i0++]);x=x*10+_-48);if(_n<46)x=-x;}while(0)
+#define ss(x) do{int i=0; while((_=_i[_i0++])<=32); for(; _>32; _= _i[_i0++]) x[i++]=_; x[i]='\0';}while(0)
+char _,_n;
+
+#define eb(a) emplace_back(a)
+#define em(a) emplace(a)
+#define sz(v) (v).size()
+#define mp(a, b) make_pair(a, b)
+#define PII pair<int, int>
+
+using namespace std;
+
+// weight/edge#, to
+vector<PII> graph[100001];
+vector<int> ans;
+priority_queue<PII, vector<PII>, greater<PII>()> pq;
+bool vis[100001];
+
+int N, M;
+
+signed main() {
+    #ifdef LOCAL
+    freopen("sample.in","r",stdin);
+    #endif
+    fread(_i,1,INPUT_SIZE,stdin);
+
+    su(N); su(M);
+
+    for (int i = 1; i <= M; i++) {
+      int u, v; su(u); su(v);
+
+      graph[u].eb({i, v});
+      graph[v].eb({i, u});
+    }
+
+    pq.em({0, 1});
+
+    while (!pq.empty() && ans.size() != N) {
+      const auto [weight, top] = pq.top(); pq.pop();
+
+      if (vis[top]) continue;
+
+      ans.eb(weight);
+
+      for (const auto &[weight, to] : graph[top]) {
+        if (!vis[to]) {
+          pq.eb({weight, to});
+        }
+      }
+    }
+
+    if (ans.size() != N) {
+        printf("Disconnected Graph")
+        return 0;
+    }
+
+    for (const auto &v : ans) {
+      if (v == 0) continue;
+      printf("%lld\n", v);
+    }
+
+    return 0;
+}
