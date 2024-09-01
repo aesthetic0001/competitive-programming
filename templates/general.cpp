8ad025d6d14e941218cd7d1711a40aadd0f21447
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-08-31 21:51:56 -0400

    add a no fastio version

--- templates/general.cpp
+++ templates/general.cpp
@@ -1,18 +1,9 @@
 #include <bits/stdc++.h>
 
-// if segfaults, try using cstdio (scanf and print) because the buffered input may be tweaking
-
 // #define int long long
 #ifdef fread_unlocked
 #define fread fread_unlocked
 #endif
-#define INPUT_SIZE (4<<20)
-int _i0=0;
-char _i[INPUT_SIZE+5];
-#define su(x) do{for(x=_i[_i0++]-48;47<(_=_i[_i0++]);x=x*10+_-48);}while(0)
-#define si(x) do{_n=_i[_i0++];if(_n-45)x=_n;else x=_i[_i0++];for(x-=48;47<(_=_i[_i0++]);x=x*10+_-48);if(_n<46)x=-x;}while(0)
-#define ss(x) do{int i=0; while((_=_i[_i0++])<=32); for(; _>32; _= _i[_i0++]) x[i++]=_; x[i]='\0';}while(0)
-char _,_n;
 
 #define eb(a) emplace_back(a)
 #define em(a) emplace(a)
@@ -26,7 +17,6 @@ signed main() {
     #ifdef LOCAL
     freopen("sample.in","r",stdin);
     #endif
-    fread(_i,1,INPUT_SIZE,stdin);
 
     return 0;
 }
--- templates/with_fastio.cpp
+++ templates/with_fastio.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 
+// if segfaults, try using cstdio (scanf and print) because the buffered input may be tweaking
+
 // #define int long long
 #ifdef fread_unlocked
 #define fread fread_unlocked
@@ -7,10 +9,8 @@
 #define INPUT_SIZE (4<<20)
 int _i0=0;
 char _i[INPUT_SIZE+5];
-#define su(x) do{while((_=_i[_i0++])<48);for(x=_-48;47<(_=_i[_i0++]);x=x*10+_-48);}while(0)
-#define si(x) do{while((_n=_i[_i0++])<45);if(_n-45)x=_n;else x=_i[_i0++];for(x-=48;47<(_=_i[_i0++]);x=x*10+_-48);if(_n<46)x=-x;}while(0)
-#define su2(x) do{for(x=_i[_i0++]-48;47<(_=_i[_i0++]);x=x*10+_-48);}while(0)
-#define si2(x) do{_n=_i[_i0++];if(_n-45)x=_n;else x=_i[_i0++];for(x-=48;47<(_=_i[_i0++]);x=x*10+_-48);if(_n<46)x=-x;}while(0)
+#define su(x) do{for(x=_i[_i0++]-48;47<(_=_i[_i0++]);x=x*10+_-48);}while(0)
+#define si(x) do{_n=_i[_i0++];if(_n-45)x=_n;else x=_i[_i0++];for(x-=48;47<(_=_i[_i0++]);x=x*10+_-48);if(_n<46)x=-x;}while(0)
 #define ss(x) do{int i=0; while((_=_i[_i0++])<=32); for(; _>32; _= _i[_i0++]) x[i++]=_; x[i]='\0';}while(0)
 char _,_n;
 
@@ -27,15 +27,6 @@ signed main() {
     freopen("sample.in","r",stdin);
     #endif
     fread(_i,1,INPUT_SIZE,stdin);
-
-    int M; su2(M);
-    string s; ss(s);
-
-    printf("%d %s\n", M, &s);
-
-    PII p = mp(1, 2);
-
-    cout << p.first << " " << p.second << "\n";
 
     return 0;
 }
