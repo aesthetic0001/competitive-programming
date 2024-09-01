df24ae9e636fbb3666f0e79f79abb0ed1a422175
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-09-01 16:31:46 -0400

    ok

--- kattis/closestpair2.cpp
+++ kattis/closestpair2.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <cstdio>
 
 // #define int long long
 #ifdef fread_unlocked
@@ -13,7 +14,6 @@
 
 using namespace std;
 
-// y, x
 int N;
 
 signed main() {
@@ -21,14 +21,21 @@ signed main() {
     freopen("sample.in","r",stdin);
     #endif
 
+    // x, y
+    vector<pair<double, double>> values;
+    // y, x
+    set<pair<double, double>, greater<pair<double, double>>> st;
+    // pair of x y points
+    pair<pair<double, double>, pair<double, double>> ans;
+    double bestDist = INFINITY;
+
     while (true) {
         scanf("%d", &N);
         if (N == 0) return 0;
-        // x, y
-        vector<pair<double, double>> values;
-        set<pair<double, double>, greater<pair<double, double>>> st;
-        double bestDist = INFINITY;
-        pair<pair<double, double>, pair<double, double>> ans;
+
+        values.clear();
+        st.clear();
+        bestDist = INFINITY;
 
         for (int j = 0; j < N; j++) {
             double x, y; scanf("%lf %lf", &x, &y);
@@ -37,40 +44,28 @@ signed main() {
 
         sort(values.begin(), values.end());
 
-        for (int j = 1; j < sz(values); j++) {
+        for (int j = 0; j < sz(values); j++) {
             const auto &[x1, y1] = values[j];
-            const auto &[prevX, prevY] = values[j - 1];
-            const double dx = x1 - prevX, dy = abs(y1 - prevY), dist = pow(dx, 2) + pow(dy, 2);
-
-            if (dist < bestDist) {
-                bestDist = dist;
-                ans.first = mp(x1, y1);
-                ans.second = mp(prevX, prevY);
-            }
-            if (dx <= bestDist && dy <= bestDist) {
-                st.em(mp(prevY, prevX));
-            }
-
-            vector<pair<double, double>> toDelete;
 
             for (const auto &[y2, x2] : st) {
-                const double dx = x1 - x2, dy = abs(y1 - y2), dist = pow(dx, 2) + pow(dy, 2);
+                printf("val %lf %lf\n", x2, y2);
+            }
 
-                if (dist > bestDist) {
-                    toDelete.eb(mp(y2, x2));
-                    continue;
-                }
+            auto ub = st.upper_bound(mp(y1 + bestDist, INFINITY));
+            auto lb = st.lower_bound(mp(y1 - bestDist, INFINITY));
 
+            for (; lb != ub; ++lb) {
+                const auto v = *lb;
+                double dist = pow(v.second - x1, 2) + pow(v.first - y1, 2);
+                printf("%lf %lf SKIBIDI\n", v.first, v.second);
                 if (dist < bestDist) {
                     bestDist = dist;
-                    ans.first = mp(x1, y1);
-                    ans.second = mp(x2, y2);
+                    ans.first = v;
+                    ans.second = values[j];
                 }
             }
 
-            for (const auto &val : toDelete) {
-                st.erase(val);
-            }
+            st.em(mp(y1, x1));
         }
 
         printf("%.2lf %.2lf %.2lf %.2lf\n", ans.first.first, ans.first.second, ans.second.first, ans.second.second);
