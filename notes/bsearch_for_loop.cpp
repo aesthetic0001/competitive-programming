03f724354a87a1ba3b1b47c5f7d54d9f09b9fd6a
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-09-26 23:10:33 -0400

    added two variations of binary search using strategic jumpsz

---
+++ notes/bsearch_for_loop.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+// #define int long long
+#ifdef fread_unlocked
+#define fread fread_unlocked
+#endif
+
+#define PII pair<int, int>
+
+using namespace std;
+
+// find the last false value
+bool monotonic(int x, int n) {
+  return pow(x, 2) > n;
+}
+
+int bsearchSqrt(int n) {
+  int idx = 0;
+  // this works, because the total area that needs to be covered is at most 2 jumps (the jump size is equal to half of the total search area)
+  for (int jumpsz = n / 2; jumpsz >= 1; jumpsz /= 2) {
+    printf("jumpsz: %d at %d\n", jumpsz, idx);
+    while (idx + jumpsz <= n && !monotonic(idx + jumpsz, n)) {
+      idx += jumpsz;
+      printf("jumping to %d\n", idx);
+    }
+  }
+
+  return idx;
+}
+
+signed main() {
+    int k;
+
+    scanf("%d", &k);
+
+    printf("sqrt of %d is %d\n", k, bsearchSqrt(k));
+
+    return 0;
+}
+
