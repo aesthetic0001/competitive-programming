17185865a90011e5b58621c314a5a8bd59f0a199
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-12-11 17:16:47 -0500

    wtf

--- CCC/cco12p2.cpp
+++ CCC/cco12p2.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <tuple>
 
 #define int long long
 #define mp(a,b) make_pair(a,b)
@@ -10,13 +11,13 @@
 #define fread fread_unlocked
 #endif
 
-#define PII pair<int, int>
+#define PII tuple<int, int, int>
 
 using namespace std;
 
 // idea: dijkstra but with two distances and we find second fastest.
-int N,M,dis1[20001],dis2[20001];
-vector<PII>g[20001];
+int N,M,dis[20001][2];
+vector<pair<int,int>>g[20001];
 priority_queue<PII,vector<PII>,greater<PII>>pq;
 
 signed main() {
@@ -25,29 +26,31 @@ signed main() {
   #endif
   scanf("%lld %lld",&N,&M);
   for(int i=1;i<=N;i++){
-    dis1[i]=INT64_MAX;
-    dis2[i]=INT64_MAX;
+    dis[i][0]=INT_MAX;
+    dis[i][1]=INT_MAX;
   }
   for(int i=0;i<M;i++){
     int u,v,w;scanf("%lld %lld %lld",&u,&v,&w);
     g[u].eb(mp(w,v));
   }
-  pq.emplace(mp(0,1));
+  pq.emplace(forward_as_tuple(0,1,0));
   while(!pq.empty()){
-    const auto[d,u]=pq.top();pq.pop();
-    /*if(d>dis1[u])continue;*/
+    const auto[d,u,flag]=pq.top();pq.pop();
+    /*if(d>dis[u])continue;*/
     for(const auto&[wV,v]:g[u]){
-      if(dis1[v]>d+wV){
-        dis1[v]=d+wV;
-        pq.emplace(mp(dis1[v],v));
-      }
-      if(d+wV>dis1[v]&&dis2[v]>d+wV){
-        dis2[v]=d+wV;
-        pq.emplace(mp(dis2[v],v));
+      if(dis[v][flag]>d+wV){
+        dis[v][flag]=d+wV;
+        pq.emplace(forward_as_tuple(d+wV,v,flag));
+      }else if (d+wV!=dis[v][flag]){
+        if(!flag){
+           if(dis[v][1]>d+wV){
+            dis[v][1]=d+wV;
+            pq.emplace(forward_as_tuple(d+wV,v,1));
+          }
+        }
       }
     }
   }
-  printf("%lld",dis2[N]==INT64_MAX?-1:dis2[N]);
+  printf("%lld",dis[N][1]==INT_MAX?-1:dis[N][1]);
   return 0;
 }
-
